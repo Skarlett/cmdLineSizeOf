96d97f048ec44f428aafa3927c0e061c0c60d34d
Alias Lyend <Lyend.chatango@gmail.com>
2016-09-19 16:11:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-chmod +x fsize.py && chmod +x sizeof && mv fsize.py /usr/local.src/ && mv sizeof /bin && rm install.sh
+chmod +x fsize.py && chmod +x sizeof && mv fsize.py /usr/local/src/ && mv sizeof /bin && rm install.sh
